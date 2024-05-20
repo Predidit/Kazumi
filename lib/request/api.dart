881f72f58650f578441672fc49dc5b8f21a9e0fe
Predidit <34627277+Predidit@users.noreply.github.com>
2024-05-20 16:48:37 +0800

    version 1.0.3

--- lib/request/api.dart
+++ lib/request/api.dart
@@ -1,6 +1,6 @@
 class Api {
   // 当前版本
-  static const String version = '1.0.2';
+  static const String version = '1.0.3';
   // 项目主页
   static const String sourceUrl = "https://github.com/Predidit/Kazumi";
   // 在线升级

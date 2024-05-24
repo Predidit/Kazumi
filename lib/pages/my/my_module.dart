b6918e980941fc3265b63ef012e1faeb62a24780
Predidit <34627277+Predidit@users.noreply.github.com>
2024-05-24 17:12:40 +0800

    add refresh rate setting

--- lib/pages/my/my_module.dart
+++ lib/pages/my/my_module.dart
@@ -6,17 +6,20 @@ import 'package:kazumi/pages/plugin_editor/plugin_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/pages/history/history_module.dart';
 import 'package:kazumi/pages/settings/theme_settings_page.dart';
+import 'package:kazumi/pages/settings/displaymode_settings.dart';
 
 class MyModule extends Module {
   @override
   void routes(r) {
     r.child("/", child: (_) => const MyPage());
     r.child("/theme", child: (_) => const ThemeSettingsPage());
-    r.child("/danmaku",
-        child: (_) => const DanmakuSettingsPage(),
-        transition: Platform.isWindows || Platform.isLinux || Platform.isMacOS
-            ? TransitionType.noTransition
-            : TransitionType.leftToRight);
+    r.child("/theme/display",
+        child: (_) => const SetDiaplayMode(),
+        transition: TransitionType.leftToRight);
+    r.child(
+      "/danmaku",
+      child: (_) => const DanmakuSettingsPage(),
+    );
     r.module("/about", module: AboutModule());
     r.module("/plugin", module: PluginModule());
     r.module("/history", module: HistoryModule());

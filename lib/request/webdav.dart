70f285b4df1798251f52327b84d01a485d053d8b
Predidit <34627277+Predidit@users.noreply.github.com>
2024-07-07 14:06:27 +0800

    add webdav interface

---
+++ lib/request/webdav.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+import 'package:webdav_client/webdav_client.dart' as webdav;
+import 'package:hive/hive.dart';
+import 'package:flutter/material.dart' show debugPrint;
+import 'package:path_provider/path_provider.dart';
+import 'package:kazumi/utils/storage.dart';
+
+class WebDav {
+  late String webDavURL;
+  late String webDavUsername;
+  late String webDavPassword;
+  late String fileName;
+  late Directory webDavLocalTempDirectory;
+  late webdav.Client client;
+
+  WebDav._internal();
+  static final WebDav _instance = WebDav._internal();
+  factory WebDav() => _instance;
+
+  Future init() async {
+    fileName = 'histories.tmp';
+    var directory = await getApplicationSupportDirectory();
+    webDavLocalTempDirectory = Directory('${directory.path}/webdavTemp');
+    Box setting = GStorage.setting;
+    webDavURL = setting.get(SettingBoxKey.webDavURL, defaultValue: '');
+    webDavUsername =
+        setting.get(SettingBoxKey.webDavUsername, defaultValue: '');
+    webDavPassword =
+        setting.get(SettingBoxKey.webDavPassword, defaultValue: '');
+    client = webdav.newClient(
+      webDavURL,
+      user: webDavUsername,
+      password: webDavPassword,
+      debug: true,
+    );
+    client.setHeaders({'accept-charset': 'utf-8'});
+    try {
+      debugPrint('webDav backup diretory not exists, creating');
+      await client.mkdir('/kazumiSync');
+      debugPrint('webDav backup diretory create success');
+    } catch (_) {
+      debugPrint('webDav backup diretory create failed');
+    }
+  }
+
+  Future update() async {
+    if (!await webDavLocalTempDirectory.exists()) {
+      await webDavLocalTempDirectory.create(recursive: true);
+    }
+    final existingFile = File('${webDavLocalTempDirectory.path}/$fileName');
+    if (await existingFile.exists()) {
+      await existingFile.delete();
+    }
+    await GStorage.backupBox('histories', existingFile.path);
+    try {
+      client.remove('/kazumiSync/$fileName.cache');
+    } catch (_) {}
+    await client.writeFromFile(existingFile.path, '/kazumiSync/$fileName.cache',
+        onProgress: (c, t) {
+      print(c / t);
+    });
+    try {
+      client.remove('/kazumiSync/$fileName');
+    } catch (_) {
+      debugPrint('webDav former backup file not exist');
+    }
+    await client.rename(
+        '/kazumiSync/$fileName.cache', '/kazumiSync/$fileName', true);
+  }
+
+  Future download() async {
+    if (!await webDavLocalTempDirectory.exists()) {
+      await webDavLocalTempDirectory.create(recursive: true);
+    }
+    final existingFile = File('${webDavLocalTempDirectory.path}/$fileName');
+    if (await existingFile.exists()) {
+      await existingFile.delete();
+    }
+    await client.read2File('/kazumiSync/$fileName', existingFile.path,
+        onProgress: (c, t) {
+      print(c / t);
+    });
+    await GStorage.restoreHistory(existingFile.path);
+  }
+}
--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:kazumi/modules/bangumi/bangumi_item.dart';
 import 'package:kazumi/modules/history/history_module.dart';
 
@@ -16,13 +19,61 @@ class GStorage {
     setting = await Hive.openBox('setting');
   }
 
+  static Future<void> backupBox(String boxName, String backupFilePath) async {
+    final appDocumentDir = await getApplicationDocumentsDirectory();
+    final hiveBoxFile = File('${appDocumentDir.path}/hive/$boxName.hive');
+    if (await hiveBoxFile.exists()) {
+      await hiveBoxFile.copy(backupFilePath);
+      print('Backup success: ${backupFilePath}');
+    } else {
+      print('Hive box not exists');
+    }
+  }
+
+  static Future<void> restoreHistory(String backupFilePath) async {
+    final appDocumentDir = await getApplicationDocumentsDirectory();
+    final backupFile = File(backupFilePath);
+    final hiveBoxFile = File('${appDocumentDir.path}/hive/histories.hive');
+    final hiveBoxLockFile = File('${appDocumentDir.path}/hive/histories.lock');
+    histories.close();
+    try {
+      await hiveBoxFile.delete();
+      try {
+        await hiveBoxLockFile.delete();
+      } catch (_) {}
+      await backupFile.copy(hiveBoxFile.path);
+    } catch (e) {
+      print('Hive box restore error: $e');
+    }
+    histories = await Hive.openBox('histories');
+  }
+
+  static Future<void> patchHistory(String backupFilePath) async {
+    final backupFile = File(backupFilePath);
+    final backupContent = await backupFile.readAsBytes();
+    final tempBox = await Hive.openBox('tempBox', bytes: backupContent);
+    final tempBoxItems = tempBox.toMap().entries;
+
+    for (var tempBoxItem in tempBoxItems) {
+      if (histories.get(tempBoxItem.key) != null) {
+        if (histories.get(tempBoxItem.key)!.lastWatchTime.isBefore(tempBoxItem.value.lastWatchTime)) {
+          histories.delete(tempBoxItem.key);
+          histories.put(tempBoxItem.key, tempBoxItem.value);
+        }
+      } else {
+        histories.put(tempBoxItem.key, tempBoxItem.value);
+      }
+    }
+    await tempBox.close();
+  }
+
   // 阻止实例化
   GStorage._();
 }
 
 class SettingBoxKey {
   static const String hAenable = 'hAenable',
-      searchEnhanceEnable = 'searchEnhanceEnable', 
+      searchEnhanceEnable = 'searchEnhanceEnable',
       autoUpdate = 'autoUpdate',
       alwaysOntop = 'alwaysOntop',
       danmakuEnhance = 'danmakuEnhance',
@@ -44,5 +95,9 @@ class SettingBoxKey {
       displayMode = 'displayMode',
       enableGitProxy = 'enableGitProxy',
       enableSystemProxy = 'enableSystemProxy',
-      isWideScreen = 'isWideScreen';
+      isWideScreen = 'isWideScreen',
+      webDavEnable = 'webDavEnable',
+      webDavURL = 'webDavURL',
+      webDavUsername = 'webDavUsername',
+      webDavPassword = 'webDavPasswd';
 }

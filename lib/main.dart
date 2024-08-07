1db197783d6c72cddf27280f8df8dc649ce90b23
Predidit <34627277+Predidit@users.noreply.github.com>
2024-08-07 11:43:02 +0800

    hide window button for macOS

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ void main() async {
       // backgroundColor: Colors.white,
       skipTaskbar: false,
       titleBarStyle: TitleBarStyle.hidden,
+      windowButtonVisibility: false,
     );
     windowManager.waitUntilReadyToShow(windowOptions, () async {
       await windowManager.show();

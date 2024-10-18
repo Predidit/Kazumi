e834123dfeafecb6ed41327839e836e78bea5e3c
ErBW_s <94068488+ErBWs@users.noreply.github.com>
2024-10-18 18:06:34 +0800

    增加macOS和iOS外部播放支持，限定使用AVPlayer，无应用选择功能

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,58 @@
 import Cocoa
 import FlutterMacOS
+import SwiftUI
+import AVKit
 
-@NSApplicationMain
+@main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    var playerView: AVPlayerView!
+    var player: AVPlayer?
+    
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        let controller : FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
+        let channel = FlutterMethodChannel.init(name: "com.predidit.kazumi/intent", binaryMessenger: controller.engine.binaryMessenger)
+        channel.setMethodCallHandler({
+            (_ call: FlutterMethodCall, _ result: FlutterResult) -> Void in
+            if call.method == "openWithMime" {
+                guard let args = call.arguments else { return }
+                if let myArgs = args as? [String: Any],
+                   let url = myArgs["url"] as? String,
+                   let mimeType = myArgs["mimeType"] as? String {
+                    self.openVideoWithMime(url: url, mimeType: mimeType)
+                }
+                result(nil)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        });
+    }
+    
+    private func openVideoWithMime(url: String, mimeType: String) {
+        let window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 1280, height: 860),
+                              styleMask: [.titled, .closable, .resizable],
+                              backing: .buffered, defer: false)
+        window.center()
+        window.makeKeyAndOrderFront(nil)
+        window.isReleasedWhenClosed = false
+        playerView = AVPlayerView(frame: window.contentView!.bounds)
+        playerView.autoresizingMask = [.width, .height]
+        window.contentView?.addSubview(playerView)
+        window.delegate = self
+        
+        let videoUrl = URL(string: url)
+        player = AVPlayer(url: videoUrl!)
+        playerView.player = player
+        playerView.player?.play()
+    }
+}
+
+extension AppDelegate: NSWindowDelegate {
+    func windowWillClose(_ notification: Notification) {
+        player?.pause()
+        player = nil
+    }
 }

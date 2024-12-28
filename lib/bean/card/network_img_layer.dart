5454b46d96c9c633b113bcee838a037cd27cede3
ErBW_s <erbws@foxmail.com>
2024-12-28 23:25:58 +0800

    add background blur

--- lib/bean/card/network_img_layer.dart
+++ lib/bean/card/network_img_layer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:kazumi/utils/constants.dart';
@@ -17,6 +19,8 @@ class NetworkImgLayer extends StatelessWidget {
     // 图片质量 默认1%
     this.quality,
     this.origAspectRatio,
+    this.sigmaX = 0,
+    this.sigmaY = 0,
   });
 
   final String? src;
@@ -27,6 +31,8 @@ class NetworkImgLayer extends StatelessWidget {
   final Duration? fadeInDuration;
   final int? quality;
   final double? origAspectRatio;
+  final double sigmaX;
+  final double sigmaY;
 
   @override
   Widget build(BuildContext context) {
@@ -60,34 +66,43 @@ class NetworkImgLayer extends StatelessWidget {
     }
 
     return src != '' && src != null
-        ? ClipRRect(
-            clipBehavior: Clip.antiAlias,
-            borderRadius: BorderRadius.circular(
-              type == 'avatar'
-                  ? 50
-                  : type == 'emote'
-                      ? 0
-                      : StyleString.imgRadius.x,
-            ),
-            child: CachedNetworkImage(
-              imageUrl: imageUrl,
-              width: width,
-              height: height,
-              memCacheWidth: memCacheWidth,
-              memCacheHeight: memCacheHeight,
-              fit: BoxFit.cover,
-              fadeOutDuration:
-                  fadeOutDuration ?? const Duration(milliseconds: 120),
-              fadeInDuration:
-                  fadeInDuration ?? const Duration(milliseconds: 120),
-              filterQuality: FilterQuality.high,
-              errorListener: (e) {
-                KazumiLogger().log(Level.warning, "网络图片加载错误 ${e.toString()}");
-              },
-              errorWidget: (BuildContext context, String url, Object error) =>
-                  placeholder(context),
-              placeholder: (BuildContext context, String url) =>
-                  placeholder(context),
+        ? CachedNetworkImage(
+            imageUrl: imageUrl,
+            width: width,
+            height: height,
+            memCacheWidth: memCacheWidth,
+            memCacheHeight: memCacheHeight,
+            fadeOutDuration:
+                fadeOutDuration ?? const Duration(milliseconds: 120),
+            fadeInDuration: fadeInDuration ?? const Duration(milliseconds: 120),
+            errorListener: (e) {
+              KazumiLogger().log(Level.warning, "网络图片加载错误 ${e.toString()}");
+            },
+            errorWidget: (BuildContext context, String url, Object error) =>
+                placeholder(context),
+            placeholder: (BuildContext context, String url) =>
+                placeholder(context),
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  filterQuality: FilterQuality.high,
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(
+                  type == 'avatar'
+                      ? 50
+                      : type == 'emote'
+                          ? 0
+                          : StyleString.imgRadius.x,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
             ),
           )
         : placeholder(context);

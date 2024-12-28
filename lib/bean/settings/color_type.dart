a7d34c4eb9b1b4d0a1c3456cf2275fbf620e3f87
ErBW_s <erbws@foxmail.com>
2024-12-29 00:13:10 +0800

    rename md3 default color

--- lib/bean/settings/color_type.dart
+++ lib/bean/settings/color_type.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 final List<Map<String, dynamic>> colorThemeTypes = [
   {'color': Colors.green, 'label': '默认'},
-  {'color': const Color(0xff6750a4), 'label': 'MD3 默认'},
   {'color': Colors.lightGreen, 'label': '浅绿色'},
   {'color': Colors.teal, 'label': '青色'},
   {'color': Colors.cyan, 'label': '蓝绿色'},
@@ -10,6 +9,7 @@ final List<Map<String, dynamic>> colorThemeTypes = [
   {'color': Colors.blue, 'label': '蓝色'},
   {'color': Colors.indigo, 'label': '靛蓝色'},
   {'color': Colors.purple, 'label': '紫色'},
+  {'color': const Color(0xff6750a4), 'label': '紫罗兰色'},
   {'color': Colors.deepPurple, 'label': '深紫色'},
   {'color': Colors.pink, 'label': '粉红色'},
   {'color': Colors.red, 'label': '红色'},

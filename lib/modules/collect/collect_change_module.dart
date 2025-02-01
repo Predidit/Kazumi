2cd4103760b80f7f437564a51604d917411faf96
Predidit <34627277+Predidit@users.noreply.github.com>
2025-02-01 22:29:29 +0800

    webdav collected bangumi sync init

---
+++ lib/modules/collect/collect_change_module.dart
@@ -0,0 +1,38 @@
+import 'package:hive/hive.dart';
+
+part 'collect_change_module.g.dart';
+
+@HiveType(typeId: 5)
+class CollectedBangumiChange {
+  // timestamp in seconds
+  @HiveField(0)
+  int id;
+
+  @HiveField(1)
+  int bangumiID;
+
+  // 1. add
+  // 2. update
+  // 3. delete
+  @HiveField(2)
+  int action;
+
+  // 1. 在看
+  // 2. 想看
+  // 3. 搁置
+  // 4. 看过
+  // 5. 抛弃
+  @HiveField(3)
+  int type;
+
+
+  @HiveField(4)
+  int timestamp;
+
+  CollectedBangumiChange(this.id, this.bangumiID, this.action,this.type, this.timestamp);
+
+  @override
+  String toString() {
+    return 'id: $id, bangumi: $bangumiID, action: $action, time: $timestamp';
+  }
+}
--- lib/utils/webdav.dart
+++ lib/utils/webdav.dart
@@ -5,6 +5,8 @@ import 'package:path_provider/path_provider.dart';
 import 'package:kazumi/utils/storage.dart';
 import 'package:logger/logger.dart';
 import 'package:kazumi/utils/logger.dart';
+import 'package:kazumi/modules/collect/collect_module.dart';
+import 'package:kazumi/modules/collect/collect_change_module.dart';
 
 class WebDav {
   late String webDavURL;
@@ -12,6 +14,8 @@ class WebDav {
   late String webDavPassword;
   late Directory webDavLocalTempDirectory;
   late webdav.Client client;
+
+  bool initialized = false;
 
   WebDav._internal();
   static final WebDav _instance = WebDav._internal();
@@ -36,6 +40,7 @@ class WebDav {
     try {
       // KazumiLogger().log(Level.warning, 'webDav backup directory not exists, creating');
       await client.mkdir('/kazumiSync');
+      initialized = true;
       KazumiLogger().log(Level.info, 'webDav backup directory create success');
     } catch (_) {
       KazumiLogger().log(Level.error, 'webDav backup directory create failed');
@@ -66,9 +71,12 @@ class WebDav {
 
   Future<void> updateCollectibles() async {
     await update('collectibles');
+    if (GStorage.collectChanges.isNotEmpty) {
+      await update('collectchanges');
+    }
   }
 
-  Future<void> downloadHistory() async {
+  Future<void> downloadAndPatchHistory() async {
     String fileName = 'histories.tmp';
     if (!await webDavLocalTempDirectory.exists()) {
       await webDavLocalTempDirectory.create(recursive: true);
@@ -97,7 +105,55 @@ class WebDav {
         onProgress: (c, t) {
       // print(c / t);
     });
-    await GStorage.patchCollectibles(existingFile.path);
+  }
+
+  Future<void> downloadCollectChanges() async {
+    String fileName = 'collectChanges.tmp';
+    if (!await webDavLocalTempDirectory.exists()) {
+      await webDavLocalTempDirectory.create(recursive: true);
+    }
+    final existingFile = File('${webDavLocalTempDirectory.path}/$fileName');
+    if (await existingFile.exists()) {
+      await existingFile.delete();
+    }
+    await client.read2File('/kazumiSync/$fileName', existingFile.path,
+        onProgress: (c, t) {
+      // print(c / t);
+    });
+  }
+
+  Future<void> syncCollectibles() async {
+    List<CollectedBangumi> remoteCollectibles = [];
+    List<CollectedBangumiChange> remoteChanges = [];
+    try {
+      await downloadCollectibles();
+    } catch (e) {
+      KazumiLogger().log(Level.error, 'webDav download collectibles failed $e');
+    }
+    try {
+      await downloadCollectChanges();
+    } catch (e) {
+      KazumiLogger()
+          .log(Level.error, 'webDav download collect changes failed $e');
+    }
+    try {
+      remoteCollectibles = await GStorage.getCollectiblesFromFile(
+          '${webDavLocalTempDirectory.path}/collectibles.tmp');
+    } catch (e) {
+      KazumiLogger()
+          .log(Level.error, 'webDav get collectibles from file failed $e');
+    }
+    try {
+      remoteChanges = await GStorage.getCollectChangesFromFile(
+          '${webDavLocalTempDirectory.path}/collectChanges.tmp');
+    } catch (e) {
+      KazumiLogger()
+          .log(Level.error, 'webDav get collect changes from file failed $e');
+    }
+    if (remoteChanges.isNotEmpty || remoteCollectibles.isNotEmpty) {
+      await GStorage.patchCollectibles(remoteCollectibles, remoteChanges);
+    }
+    await updateCollectibles();
   }
 
   Future<void> ping() async {

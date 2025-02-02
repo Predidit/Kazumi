a150d3d91e5eb31b5a2ba97309eca356438a123e
ErBW_s <erbws@foxmail.com>
2025-02-02 17:25:22 +0800

    Done

--- lib/bbcode/README.md
+++ lib/bbcode/README.md
@@ -7,8 +7,9 @@
 
 ## 关键文件
 
-- [lib/bbcode/BBCodeBaseListener.dart](generated/BBCodeBaseListener.dart): antlr4 生成的 Listener 抽象类空实现
-- [lib/bbcode/bbcode.dart](bbcode.dart): BBCode 解析器的入口文件
+- [lib/bbcode/bbcode_elements.dart](bbcode_elements.dart): BBCode 元素
+- [lib/bbcode/bbcode_base_listener.dart](bbcode_base_listener.dart): BBCode 解析器的入口文件
+- [lib/bbcode/bbcode_widget.dart](bbcode_widget.dart): BBCode 组件
 
 ## 如何开发
 
@@ -20,11 +21,40 @@
 ### 开发
 
 1. 修改 [assets/bbcode/BBCode.g4](../../assets/bbcode/BBCode.g4) 文件，通过插件的 Preview 功能确定解析是否正确
-2. 通过该文件生成新的 dart 文件到 [lib/bbcode/generated](../../lib/bbcode/generated) 文件夹内
-3. 将 [lib/bbcode/BBCodeBaseListener.dart](generated/BBCodeBaseListener.dart) 中的空实现复制到 [lib/bbcode/bbcode.dart](bbcode.dart) 中，参考文件内的注释进行修改
+2. 通过该文件生成新的 dart 文件到 [lib/bbcode/generated](../../lib/bbcode/generated) 文件夹内，删除无用文件
+3. 参考文件内的注释进行修改
 
 ### 测试 BBCode
 
-```
-[url=https://bangumi.tv/blog/348736]测试链接[/url][url]https://bangumi.tv/blog/348736[/url][quote][b]用户[/b]测试表情(bgm35)[/quote]测试换行\t\n测试特殊符号[]()测试字符表情(TAT)哈哈哈哈[img]https://p.inari.site/guest/25-01/22/67907598b3a74.jpg[/img]
+```dart
+import 'package:flutter/material.dart';
+import 'bbcode/bbcode_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('BBCode Parser')),
+        body: Card(
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: BBCodeWidget(
+                bbcode:
+                '[quote][b]用户[/b]说：[s]测试表情和删除线(bgm35)[/s][/quote]\n[mask]测试特殊符号[]()测试字符表情(TAT)[/mask][url=https://bangumi.tv/blog/348736]测试链接[/url][url]https://bangumi.tv/blog/348736[/url][img]https://bangumi.tv/img/rc3/logo_2x.png[/img]\n\n[color=grey][size=10][来自Bangumi for android] [url=https://bgm.tv/group/topic/350677][color=grey]获取[/color][/url][/size][/color]',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 ```
--- lib/bean/card/episode_comments_card.dart
+++ lib/bean/card/episode_comments_card.dart
@@ -1,11 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:kazumi/bbcode/bbcode.dart';
-import 'package:kazumi/bean/dialog/dialog_helper.dart';
+import 'package:kazumi/bbcode/bbcode_widget.dart';
 import 'package:kazumi/modules/comments/comment_item.dart';
 import 'package:kazumi/utils/utils.dart';
-import 'package:styled_text/styled_text.dart';
 
 class EpisodeCommentsCard extends StatelessWidget {
   const EpisodeCommentsCard({
@@ -82,92 +78,10 @@ class EpisodeCommentsCard extends StatelessWidget {
                     ),
                   );
                 },
-              )
+              ),
           ],
         ),
       ),
     );
-  }
-
-  Widget commentsWithStyledText(String comment, BuildContext context) {
-    return StyledText(text: comment, tags: {
-      'b': StyledTextTag(
-        style: const TextStyle(fontWeight: FontWeight.bold),
-      ),
-      'i': StyledTextTag(
-        style: const TextStyle(fontStyle: FontStyle.italic),
-      ),
-      'q': StyledTextTag(
-        style: TextStyle(color: Theme.of(context).colorScheme.outline),
-      ),
-      'format_quote': StyledTextIconTag(Icons.format_quote,
-          color: Theme.of(context).colorScheme.outline,
-          alignment: PlaceholderAlignment.top),
-      's': StyledTextTag(
-        style: const TextStyle(decoration: TextDecoration.lineThrough),
-      ),
-      'u': StyledTextTag(
-        style: const TextStyle(decoration: TextDecoration.underline),
-      ),
-      'image': StyledTextWidgetBuilderTag(
-        (_, attributes, textContent) {
-          return CachedNetworkImage(
-            imageUrl: textContent!,
-            placeholder: (context, url) => const SizedBox(
-              width: 14,
-              height: 14,
-            ),
-            errorWidget: (context, error, stackTrace) {
-              return const Text('.');
-            },
-          );
-        },
-      ),
-      'link': StyledTextActionTag(
-        (_, attrs) {
-          _copyLink(attrs);
-        },
-        style: const TextStyle(color: Colors.blue),
-      ),
-      'color': StyledTextCustomTag(
-          baseStyle: const TextStyle(fontStyle: FontStyle.normal),
-          parse: (baseStyle, attributes) {
-            if (attributes.containsKey('color')) {
-              final String color = attributes['color']!;
-              switch (color) {
-                case 'red':
-                  return baseStyle?.copyWith(color: Colors.red);
-                case 'blue':
-                  return baseStyle?.copyWith(color: Colors.blue);
-                case 'orange':
-                  return baseStyle?.copyWith(color: Colors.orange);
-                case 'green':
-                  return baseStyle?.copyWith(color: Colors.green);
-                case 'grey':
-                  return baseStyle?.copyWith(color: Colors.grey);
-                default:
-                  return baseStyle;
-              }
-            } else {
-              return baseStyle;
-            }
-          }),
-      'size': StyledTextCustomTag(
-          baseStyle: const TextStyle(fontStyle: FontStyle.normal),
-          parse: (baseStyle, attributes) {
-            if (attributes.containsKey('size')) {
-              double size = double.tryParse(attributes['size']!) ?? 14;
-              return baseStyle?.copyWith(fontSize: size);
-            } else {
-              return baseStyle;
-            }
-          }),
-    });
-  }
-
-  Future<void> _copyLink(Map<String?, String?> attrs) async {
-    final String? link = attrs['href'];
-    await Clipboard.setData(ClipboardData(text: link!));
-    KazumiDialog.showToast(message: '已复制链接到剪贴板');
   }
 }

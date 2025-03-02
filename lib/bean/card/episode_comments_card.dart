54534dd73301bfeb3a8ef5f89240b0473d152173
ErBW_s <erbws@foxmail.com>
2025-03-02 11:34:42 +0800

    fix comments replies has wierd bottom padding

--- lib/bean/card/episode_comments_card.dart
+++ lib/bean/card/episode_comments_card.dart
@@ -40,6 +40,9 @@ class EpisodeCommentsCard extends StatelessWidget {
             BBCodeWidget(bbcode: commentItem.comment.comment),
             if (commentItem.replies.isNotEmpty)
               ListView.builder(
+                // Don't know why but ohos has bottom padding,
+                // needs to set to 0 manually.
+                padding: const EdgeInsets.only(bottom: 0),
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
                 itemCount: commentItem.replies.length,

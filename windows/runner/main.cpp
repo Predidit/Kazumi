42f6982ec1f03d10360d40f83eb73af8db398307
Predidit <34627277+Predidit@users.noreply.github.com>
2025-03-20 14:39:33 +0800

    better storage error page

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -6,21 +6,50 @@
 #include "utils.h"
 
 // recommended by NVIDIA to enable high-performance GPU
-extern "C" {
+extern "C"
+{
   __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 }
 
 // recommended by AMD to enable high-performance GPU
-extern "C" {
+extern "C"
+{
   __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
 
+HANDLE mutex = NULL;
+
+bool isSingleInstance()
+{
+  if (mutex != NULL)
+  {
+    return true;
+  }
+  std::wstring mutex_str = L"kazumi.win.mutex";
+  mutex = ::CreateMutex(NULL, TRUE, mutex_str.c_str());
+  if (mutex == NULL || GetLastError() == ERROR_ALREADY_EXISTS)
+  {
+    CloseHandle(mutex);
+    mutex = NULL;
+    return false;
+  }
+  return true;
+}
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
+                      _In_ wchar_t *command_line, _In_ int show_command)
+{
+  // Make sure the application is a single instance.
+  // This is important for the application to work correctly with the local storage.
+  if (!isSingleInstance())
+  {
+    MessageBox(NULL, L"Another instance is already running.", L"Error", MB_OK | MB_ICONERROR);
+    return EXIT_FAILURE;
+  }
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent())
+  {
     CreateAndAttachConsole();
   }
 
@@ -38,17 +67,25 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"kazumi", origin, size)) {
+  if (!window.Create(L"kazumi", origin, size))
+  {
+    if (mutex) {
+      CloseHandle(mutex);
+    }
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
 
   ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
+  while (::GetMessage(&msg, nullptr, 0, 0))
+  {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
   }
 
   ::CoUninitialize();
+  if (mutex) {
+    CloseHandle(mutex);
+  }
   return EXIT_SUCCESS;
 }

8a2396c4380eb646c16a3165213618c76e1582d8
Predidit <34627277+Predidit@users.noreply.github.com>
2025-09-05 05:21:54 +0800

    refactor: remove android webview video player to improve performance

--- lib/pages/webview/webview_item_impel/webview_android_item_impel.dart
+++ lib/pages/webview/webview_item_impel/webview_android_item_impel.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kazumi/pages/webview/webview_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/pages/webview/webview_controller_impel/webview_android_controller_impel.dart';
-import 'package:kazumi/utils/utils.dart';
-import 'package:flutter_inappwebview_platform_interface/flutter_inappwebview_platform_interface.dart';
 
 class WebviewAndroidItemImpel extends StatefulWidget {
   const WebviewAndroidItemImpel({super.key});
@@ -20,6 +18,7 @@ class _WebviewAndroidItemImpelState extends State<WebviewAndroidItemImpel> {
   @override
   void initState() {
     super.initState();
+    webviewAndroidItemController.init();
   }
 
   @override
@@ -30,34 +29,10 @@ class _WebviewAndroidItemImpelState extends State<WebviewAndroidItemImpel> {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformInAppWebViewWidget(PlatformInAppWebViewWidgetCreationParams(
-      initialSettings: InAppWebViewSettings(
-        userAgent: Utils.getRandomUA(),
-        mediaPlaybackRequiresUserGesture: true,
-        cacheEnabled: false,
-        blockNetworkImage: true,
-        loadsImagesAutomatically: false,
-        upgradeKnownHostsToHTTPS: false,
-        safeBrowsingEnabled: false,
-        mixedContentMode: MixedContentMode.MIXED_CONTENT_COMPATIBILITY_MODE,
-        geolocationEnabled: false,
-      ),
-      onWebViewCreated: (controller) {
-        debugPrint('[WebView] Created');
-        webviewAndroidItemController.webviewController = controller;
-        webviewAndroidItemController.init();
-      },
-      onLoadStart: (controller, url) async {
-        webviewAndroidItemController.logEventController
-            .add('started loading: $url');
-        if (url.toString() != 'about:blank') {
-          await webviewAndroidItemController.onLoadStart();
-        }
-      },
-      onLoadStop: (controller, url) {
-        webviewAndroidItemController.logEventController
-            .add('loading completed: $url');
-      },
-    )).build(context);
+    return Container(
+        height: MediaQuery.of(context).size.width * 9.0 / (16.0),
+        width: MediaQuery.of(context).size.width,
+        color: Colors.black,
+        child: const Center(child: Text('此平台不支持Webview规则', style: TextStyle(color: Colors.white))));
   }
 }

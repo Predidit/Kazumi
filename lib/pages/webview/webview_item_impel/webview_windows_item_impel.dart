abb9f62845d1b680a3be947d927653484ae27dff
Predidit <34627277+Predidit@users.noreply.github.com>
2025-09-20 17:02:48 +0800

    clean: remove webview player for windows platform

--- lib/pages/webview/webview_item_impel/webview_windows_item_impel.dart
+++ lib/pages/webview/webview_item_impel/webview_windows_item_impel.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:webview_windows/webview_windows.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:window_manager/window_manager.dart';
 import 'package:kazumi/pages/video/video_controller.dart';
 import 'package:kazumi/pages/webview/webview_controller.dart';
 
@@ -15,7 +12,6 @@ class WebviewWindowsItemImpel extends StatefulWidget {
 }
 
 class _WebviewWindowsItemImpelState extends State<WebviewWindowsItemImpel> {
-  final List<StreamSubscription> _subscriptions = [];
   final webviewDesktopItemController = Modular.get<WebviewItemController>();
   final VideoPageController videoPageController =
       Modular.get<VideoPageController>();
@@ -23,53 +19,21 @@ class _WebviewWindowsItemImpelState extends State<WebviewWindowsItemImpel> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    webviewDesktopItemController.init();
   }
 
   @override
   void dispose() {
-    for (var s in _subscriptions) {
-      try {
-        s.cancel();
-      } catch (_) {}
-    }
     webviewDesktopItemController.dispose();
     super.dispose();
   }
 
-  Future<void> initPlatformState() async {
-    // 初始化Webview
-    if (webviewDesktopItemController.webviewController == null) {
-      await webviewDesktopItemController.init();
-    }
-    // 接受全屏事件
-    _subscriptions.add(webviewDesktopItemController
-        .webviewController.containsFullScreenElementChanged
-        .listen((flag) {
-      videoPageController.isFullscreen = flag;
-      windowManager.setFullScreen(flag);
-    }));
-    if (!mounted) return;
-
-    setState(() {});
-  }
-
-  Widget get compositeView {
-    if (webviewDesktopItemController.webviewController == null) {
-      return const Text(
-        'Not Initialized',
-        style: TextStyle(
-          fontSize: 24.0,
-          fontWeight: FontWeight.w900,
-        ),
-      );
-    } else {
-      return Webview(webviewDesktopItemController.webviewController);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return compositeView;
+    return Container(
+        height: MediaQuery.of(context).size.width * 9.0 / (16.0),
+        width: MediaQuery.of(context).size.width,
+        color: Colors.black,
+        child: const Center(child: Text('此平台不支持Webview规则')));
   }
 }

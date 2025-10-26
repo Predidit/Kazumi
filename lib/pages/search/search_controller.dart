f542f63610236f178ab3fb11c0e0cf876ba07c5d
Ferne <a3230269751@163.com>
2025-10-26 16:19:23 +0800

    [Added]: 过滤了标注已看过的番剧（#1413）

--- lib/pages/search/search_controller.dart
+++ lib/pages/search/search_controller.dart
@@ -13,12 +13,17 @@ class SearchPageController = _SearchPageController with _$SearchPageController;
 abstract class _SearchPageController with Store {
   final Box setting = GStorage.setting;
   final Box searchHistoryBox = GStorage.searchHistory;
+  final Box collectiblesBox = GStorage.collectibles;
 
   @observable
   bool isLoading = false;
 
   @observable
   bool isTimeOut = false;
+
+  @observable
+  late bool notShowWatchedBangumis =
+      setting.get(SettingBoxKey.searchNotShowWatchedBangumis, defaultValue: false);
 
   @observable
   ObservableList<BangumiItem> bangumiList = ObservableList.of([]);
@@ -106,5 +111,19 @@ abstract class _SearchPageController with Store {
   Future<void> clearSearchHistory() async {
     await searchHistoryBox.clear();
     loadSearchHistories();
+  }
+
+  @action
+  Future<void> setNotShowWatchedBangumis(bool value) async {
+    notShowWatchedBangumis = value;
+    await setting.put(SettingBoxKey.searchNotShowWatchedBangumis, value);
+  }
+
+  @action
+  Set<String> loadWatchedBangumiNames() {
+    return collectiblesBox.values
+        .where((item) => item.type == 4)
+        .map((item) => item.bangumiItem.name.toString())
+        .toSet();
   }
 }

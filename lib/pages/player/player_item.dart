2bcaafaa7a02efab2d04147b4970094eeb69337b
林晨 <19362246+0Chencc@users.noreply.github.com>
2025-11-06 20:47:05 +0800

    新增了一个功能允许设置弹幕与播放速度是否一致
    现在可以在播放时设置弹幕播放速度是否跟视频倍速一致，也可以在设置-弹幕设置中管理，两个选项是同步的。

--- lib/pages/player/player_item.dart
+++ lib/pages/player/player_item.dart
@@ -94,6 +94,7 @@ class _PlayerItemState extends State<PlayerItem>
   late bool _danmakuDanDanSource;
   late double _danmakuDuration;
   late int _danmakuFontWeight;
+  late bool _danmakuFollowSpeed;
 
   // 硬件解码
   late bool haEnable;
@@ -1009,6 +1010,8 @@ class _PlayerItemState extends State<PlayerItem>
         setting.get(SettingBoxKey.danmakuDanDanSource, defaultValue: true);
     _danmakuFontWeight =
         setting.get(SettingBoxKey.danmakuFontWeight, defaultValue: 4);
+    _danmakuFollowSpeed =
+        setting.get(SettingBoxKey.danmakuFollowSpeed, defaultValue: true);
     haEnable = setting.get(SettingBoxKey.hAenable, defaultValue: true);
     playerTimer = getPlayerTimer();
     windowManager.addListener(this);

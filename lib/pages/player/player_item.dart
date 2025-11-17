bb338c251b61e36762279dea4ecf3aa95d4ca1d1
Predidit <34627277+Predidit@users.noreply.github.com>
2025-11-17 15:01:46 +0800

    chore: better desktop compact layout

--- lib/pages/player/player_item.dart
+++ lib/pages/player/player_item.dart
@@ -951,9 +951,18 @@ class _PlayerItemState extends State<PlayerItem>
       return false;
     }
     // does not meet Google's phone landscape height and tablet landscape width requirements.
-    if (MediaQuery.sizeOf(context).height >
-            LayoutBreakpoint.compact['height']! &&
-        MediaQuery.sizeOf(context).width < LayoutBreakpoint.medium['width']!) {
+    if (!Utils.isDesktop() &&
+        (MediaQuery.sizeOf(context).height >
+                LayoutBreakpoint.compact['height']! &&
+            MediaQuery.sizeOf(context).width <
+                LayoutBreakpoint.medium['width']!)) {
+      return false;
+    }
+    if (Utils.isDesktop() &&
+        (MediaQuery.sizeOf(context).height >
+                LayoutBreakpoint.compact['height']! &&
+            MediaQuery.sizeOf(context).width <
+                LayoutBreakpoint.compact['width']!)) {
       return false;
     }
     return true;
@@ -1000,7 +1009,8 @@ class _PlayerItemState extends State<PlayerItem>
     _massiveMode =
         setting.get(SettingBoxKey.danmakuMassive, defaultValue: false);
     _danmakuColor = setting.get(SettingBoxKey.danmakuColor, defaultValue: true);
-    _danmakuDuration = setting.get(SettingBoxKey.danmakuDuration, defaultValue: 8.0);
+    _danmakuDuration =
+        setting.get(SettingBoxKey.danmakuDuration, defaultValue: 8.0);
     _danmakuBiliBiliSource =
         setting.get(SettingBoxKey.danmakuBiliBiliSource, defaultValue: true);
     _danmakuGamerSource =
@@ -1273,7 +1283,8 @@ class _PlayerItemState extends State<PlayerItem>
                           area: _danmakuArea,
                           opacity: _opacity,
                           fontSize: _fontSize,
-                          duration: _danmakuDuration ~/ playerController.playerSpeed,
+                          duration:
+                              _danmakuDuration ~/ playerController.playerSpeed,
                           showStroke: _border,
                           fontWeight: _danmakuFontWeight,
                           massiveMode: _massiveMode,

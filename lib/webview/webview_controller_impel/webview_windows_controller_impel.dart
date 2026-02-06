782479c492371945442d0014c45846188ab4c556
Predidit <34627277+Predidit@users.noreply.github.com>
2026-02-06 21:42:45 +0800

    fix(windows): fix subscriptions leak in webview controller

--- lib/webview/webview_controller_impel/webview_windows_controller_impel.dart
+++ lib/webview/webview_controller_impel/webview_windows_controller_impel.dart
@@ -90,6 +90,7 @@ class WebviewWindowsItemControllerImpel
         s.cancel();
       } catch (_) {}
     });
+    subscriptions.clear();
     await redirect2Blank();
   }
 
@@ -100,6 +101,7 @@ class WebviewWindowsItemControllerImpel
         s.cancel();
       } catch (_) {}
     });
+    subscriptions.clear();
     // disposeEnvironment() 内部已通过 headless_instances_.clear() 销毁所有 headless 实例，
     // 不要额外调用 headlessWebview.dispose()，否则两个异步方法通道调用可能产生竞争，
     // 导致 disposeEnvironment 先到达原生侧清除实例后，disposeHeadless 找不到条目抛出异常。

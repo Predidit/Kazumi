d94809554b03f52a15e65ae624a4060a9637c0f5
LiggMax <wenzhouli06@gmail.com>
2026-04-21 20:04:19 +0800

    refactor(episode_comments_card): 优化剧集评论卡片布局结构
    - 修改为左右内容布局（左侧用户头像，右侧用户昵称、发表时间、评论内容）
    - 优化发表时间数据展示效果（使用主题浅灰色、文字缩小）
    - 移除回复评论之间的Divider(分割线)，个人觉得缩进的回复内容展示已经可以明确评论之间的回复关系不需要分割线，反而分割线会影响美观

--- lib/bean/card/episode_comments_card.dart
+++ lib/bean/card/episode_comments_card.dart
@@ -27,67 +27,79 @@ class EpisodeCommentsCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CircleAvatar(
                   backgroundImage:
                       NetworkImage(commentItem.comment.user.avatar.large),
                 ),
                 const SizedBox(width: 8),
-                Column(
+                Expanded(child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(commentItem.comment.user.nickname),
-                    Text(Utils.dateFormat(commentItem.comment.createdAt)),
+                    Text(
+                      Utils.dateFormat(commentItem.comment.createdAt),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Theme.of(context).colorScheme.outline,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    BBCodeWidget(bbcode: userComment),
+                    if (commentItem.replies.isNotEmpty)
+                      ListView.builder(
+                        // Don't know why but ohos has bottom padding,
+                        // needs to set to 0 manually.
+                        padding: const EdgeInsets.only(bottom: 0),
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: commentItem.replies.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(left: 48),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                          commentItem.replies[index].user.avatar.large),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Expanded(child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(commentItem.replies[index].user.nickname),
+                                        Text(
+                                          Utils.dateFormat(
+                                              commentItem.replies[index].createdAt),
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Theme.of(context).colorScheme.outline,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 8),
+                                        BBCodeWidget(
+                                            bbcode: commentItem.replies[index].comment),
+                                      ],
+                                    ),
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            ),
+                          );
+                        },
+                      ),
                   ],
+                ),
                 ),
               ],
             ),
-            const SizedBox(height: 8),
-            BBCodeWidget(bbcode: userComment),
-            if (commentItem.replies.isNotEmpty)
-              ListView.builder(
-                // Don't know why but ohos has bottom padding,
-                // needs to set to 0 manually.
-                padding: const EdgeInsets.only(bottom: 0),
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: commentItem.replies.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(left: 48),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Divider(
-                          color: Theme.of(context).dividerColor.withAlpha(60),
-                        ),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  commentItem.replies[index].user.avatar.large),
-                            ),
-                            const SizedBox(width: 8),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(commentItem.replies[index].user.nickname),
-                                Text(
-                                  Utils.dateFormat(
-                                      commentItem.replies[index].createdAt),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        BBCodeWidget(
-                            bbcode: commentItem.replies[index].comment),
-                      ],
-                    ),
-                  );
-                },
-              ),
           ],
         ),
       ),

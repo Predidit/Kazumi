f87d7d752616e4c352c82ffededb5dddf8f8bca6
LiggMax <wenzhouli06@gmail.com>
2026-04-23 20:18:52 +0800

    anime_image_search

---
+++ lib/pages/anime_image_search/anime_image_search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimeImageSearchPage extends StatefulWidget {
+  const AnimeImageSearchPage({super.key});
+
+  @override
+  State<AnimeImageSearchPage> createState() => _AnimeImageSearchPageState();
+}
+
+class _AnimeImageSearchPageState extends State<AnimeImageSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

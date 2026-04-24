f4772c47e509a00353ffcd15e8a7d4496364e1fe
dom <githubaccount56556@proton.me>
2026-04-24 13:31:43 +0800

    use cached_network_image_ce
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/bean/widget/image_preview.dart
+++ lib/bean/widget/image_preview.dart
@@ -1,4 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cached_network_image_ce/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:photo_view/photo_view.dart';

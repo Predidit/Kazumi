21778036f7617a8efe5ede3cf194180078a897b8
HX <im@bgzo.cc>
2026-04-26 13:51:21 +0800

    chore(bangumi): clean code

--- lib/pages/collect/collect_controller.dart
+++ lib/pages/collect/collect_controller.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:url_launcher/url_launcher.dart';

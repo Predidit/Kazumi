0f268092955deb12e4c470d4a845b16103624718
ZHwash <ac812057812@qq.com>
2026-06-05 13:21:11 +0800

    revert: restore build configs to upstream main state

--- android/build.gradle
+++ android/build.gradle
@@ -1,7 +1,5 @@
 allprojects {
     repositories {
-        maven { url 'https://maven.aliyun.com/repository/google' }
-        maven { url 'https://maven.aliyun.com/repository/central' }
         google()
         mavenCentral()
     }

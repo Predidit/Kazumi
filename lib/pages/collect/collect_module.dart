170ad6008cd36344ca8305eb0ee0bd5802d4fc51
Predidit <34627277+Predidit@users.noreply.github.com>
2026-09-05 18:39:05 +0800

    Revert "draft"
    This reverts commit b13cc83e58157d93651030555a1577b216492950.

--- lib/pages/collect/collect_module.dart
+++ lib/pages/collect/collect_module.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/pages/collect/collect_page.dart';
 import 'package:kazumi/pages/collect/collect_controller.dart';
-import 'package:kazumi/repositories/history_repository.dart';
 
 final collectModule = createModule(
   path: '/collect',
@@ -11,7 +10,6 @@ final collectModule = createModule(
       transition: TransitionType.none,
       child: (context, state) => CollectPage(
         controller: inject<CollectController>(),
-        historyRepository: inject<IHistoryRepository>(),
       ),
     );
   },
